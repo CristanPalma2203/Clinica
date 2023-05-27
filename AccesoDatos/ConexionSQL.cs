427bc01f8590463abde0f2960f7875fbc987f0cb
JosueFlores777 <aaaaaveroo@gmail.com>
2023-05-27 11:52:55 -0600

    conection changes

--- AccesoDatos/ConexionSQL.cs
+++ AccesoDatos/ConexionSQL.cs
@@ -14,7 +14,7 @@ namespace AccesoDatos
         public ConexionSQL()
         {
             //CadenaConexion = "Server=DESKTOP-G30J10N;DataBase=SistemaClinico; integrated security = true";
-            CadenaConexion = "Server=DESKTOP-NOE5NKV;DataBase=SistemaClinico; integrated security = true";
+            CadenaConexion = "Server = 3.228.164.208; Database = SistemaClinico; User Id = sa; Password = SapiAdmin2020";
         }
         protected SqlConnection GetConnection()
         {
